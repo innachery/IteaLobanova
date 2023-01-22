b990666558901c931b5bb188d3f43cec74440af7
Inna Lobanova <inna.chery@gmail.com>
2023-01-22 11:40:37 +0100

    Homework #1

---
+++ src/MyClass.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public static int year = 2023;
+        public static int sumOfTwoNumbers(int a, int b){
+            return a+b;
+    }
+    public static void main(String[] args) {
+        System.out.println(sumOfTwoNumbers(5,12));
+        System.out.println(year);
+    }
+}
+
