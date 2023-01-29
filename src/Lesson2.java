0986382a7f70c8d873e48c250104c6b3e277dcaf
Inna Lobanova <inna.chery@gmail.com>
2023-01-29 09:31:26 +0100

    Homework #2 with changes

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -2,28 +2,23 @@ import java.util.Scanner;
 
 public class Lesson2 {
     public static void main(String[] args) {
-        System.out.print("Введіть трьохзначне число: ");
+        System.out.println("Сума цифр трьохзначного числа складає " + actionsWithNumbers(117));
+        System.out.println("Сума довжин двох слів складає: " + actionsWithNumbers("привіт", "папа"));
+        System.out.print("Введіть число: ");
         Scanner inputNum = new Scanner(System.in);
-        int i = inputNum.nextInt();
-        if (i > 99 && i < 1000) {
-            System.out.println("Сума цифр числа: " + sumOfDigits(i));
-        } else {
-            System.out.println("Ви ввели не трьохзначне число. Спробуйте ще раз");
-        }
-        sumOfDigits("мама", "папа");
-        System.out.println(sumOfDigits(-7.00));
+        double i = inputNum.nextDouble();
+        System.out.println(actionsWithNumbers(i));
     }
 
-    public static int sumOfDigits(int i) {
+    public static int actionsWithNumbers(int i) {
         return (i % 10 + (i / 10) % 10 + i / 100);
     }
 
-    public static String sumOfDigits(String word1, String word2) {
-        System.out.println(word1 + word2);
-        return word1 + word2;
+    public static int actionsWithNumbers(String word1, String word2) {
+        return word1.length() + word2.length();
     }
 
-    public static boolean sumOfDigits(double i) {
+    public static boolean actionsWithNumbers(double i) {
         if (i > 0) {
             System.out.println("Число більше 0");
             return true;
